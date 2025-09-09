1acba975e52a98dd46de6b09c940f791002f1899
Student priyakumariyadav99 <priyakumariyadav219@gmail.com>
2025-09-09 22:13:56 +0530

    form validations added

--- src/main/java/in/ashokit/controller/ProductController.java
+++ src/main/java/in/ashokit/controller/ProductController.java
@@ -2,10 +2,12 @@ package in.ashokit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import in.ashokit.binding.Product;
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
@@ -19,9 +21,15 @@ public class ProductController {
 	
 	// message to handle the submission
 	@PostMapping("/product")
-	public String handleSubmissionBtn(Product p, Model model) {
+	public String handleSubmissionBtn(@Valid Product p, BindingResult result, Model model) {
 		System.out.println(p);
-		model.addAttribute("msg", "Product saved");
+		if(result.hasErrors()) {
+			return "index";
+		}else {
+			model.addAttribute("msg", "Product saved");
+			
+		}
+		
 		return "index";
 	}
 
